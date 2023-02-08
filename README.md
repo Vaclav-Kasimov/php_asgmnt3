5a0a29bfdd1c1c7d1fc4b611f853661537f57a73
Vaclav-Kasimov <kasimov.vyacheslav.aaa@gmail.com>
2023-02-08 21:01:58 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # php_asgmnt3
+https://www.wa4e.com/assn/guess/
---
+++ guess.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kasimov Viacheslav ccd19880</title>
+    </head>
+    <body>
+        <h1>Welcome to my guessing game!</h1>
+        <p>
+            <?php
+            $correct = 53;
+            if ( !(isset($_GET['guess'])) ){
+                echo "Missing guess parameter";
+            } elseif ( $_GET['guess'] === null){
+                echo "Your guess is too short";
+            } elseif ( !(is_numeric($_GET['guess']))){
+                echo "Your guess is not a number";
+            } elseif ( $_GET['guess'] < $correct ){
+                echo "Your guess is too low";
+            } elseif ( $_GET['guess'] > $correct ){
+                echo "Your guess is too high";
+            } else {
+                echo "Congratulations - You are right";
+            }
+            ?>
+        </p>
+    </body>
+</html>
